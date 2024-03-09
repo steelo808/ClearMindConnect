e5392c4389ea299eb69f95309790d903c0eba3de
Sterling Newsom <sterlingnewsom808@gmail.com>
2024-03-09 01:47:45 -0500

    implemented vanta.js for background

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("working");
+
+document.getElementById('toggle-menu').addEventListener('click', function() {
+    const menuItems = document.getElementById('menu-items');
+    menuItems.classList.toggle('show');
+  });
