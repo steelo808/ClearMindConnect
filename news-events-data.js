04dbe5dbc916c9e8737c5ec31af671de4d6885cf
Sterling Newsom <sterlingnewsom808@gmail.com>
2024-03-13 01:09:36 -0400

    working on new section

---
+++ news-events-data.js
@@ -0,0 +1,5 @@
+console.log("working");
+
+apiKey = 'c05688524a534b2786c1f41c1f2d7fa5';
+
+apiUrl = "https://newsapi.org/v2/everything?q=Dementia&from=2024-02-10&sortBy=publishedAt&apiKey=c05688524a534b2786c1f41c1f2d7fa5"
