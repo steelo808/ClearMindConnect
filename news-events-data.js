41ad0dcd20b81cd95296d8c2125f909e7434f880
Sterling Newsom <sterlingnewsom808@gmail.com>
2024-03-13 22:34:40 -0400

    Working on news-events-data

--- news-events-data.js
+++ news-events-data.js
@@ -1,5 +1,65 @@
 console.log("working");
 
-apiKey = 'c05688524a534b2786c1f41c1f2d7fa5';
+// const $apiUrl = "https://newsapi.org/v2/everything";
 
-apiUrl = "https://newsapi.org/v2/everything?q=Dementia&from=2024-02-10&sortBy=publishedAt&apiKey=c05688524a534b2786c1f41c1f2d7fa5"
+
+// const $params = {
+//     q: 'alzheimers',
+//     from:"2024-02-10",
+//     sortBy:"popularity",
+//     apiKey:"c05688524a534b2786c1f41c1f2d7fa5",
+//     PageSize: 3
+// }
+
+// const $queryString = new URLSearchParams($params).toString();
+
+// $.get(`${$apiUrl}?${$queryString}`, (data) =>{
+//     console.log(data)
+//     })
+
+    // for(let i = 0; i < data.length; i++){
+    //     const $news = data.articles;
+    //     const $newsCard = $(".news-card");
+    //     const $content = $(".content");
+    //     // const $img = $(`<img src=`)
+
+    // }
+
+// $.ajax({
+//     url:"https://newsapi.org/v2/everything",
+//     method: 'GET',
+//     data: {
+//         q: 'alzheimers',
+//         from:"2024-02-10",
+//         sortBy:"popularity",
+//         apiKey:"c05688524a534b2786c1f41c1f2d7fa5",
+//         PageSize: 3
+//     },
+//     success: function(data){
+//         console.log(data)
+//     },
+//     error: function(xhr, status, error) {
+//         console.error(error)
+//     }
+
+
+// })
+
+var url = 'https://newsapi.org/v2/everything?' +
+          'q=Apple&' +
+          'from=2024-03-14&' +
+          'sortBy=popularity&' +
+          'apiKey=c05688524a534b2786c1f41c1f2d7fa5';
+
+var req = new Request(url);
+
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
+    .then(function(data){
+        console.log(data)
+    })
+    .catch(function(error) {
+        console.error(error)
+    })
